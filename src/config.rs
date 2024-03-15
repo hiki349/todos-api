522b8b694e6bc71cf2651f91148864a0711182e4
hiki349 <vladyakima@gmail.com>
2024-03-15 23:52:36 +0200

    added docker-compose

--- src/config.rs
+++ src/config.rs
@@ -2,12 +2,14 @@ use std::env;
 
 pub struct Config {
     pub addr: String,
+    pub db_conn: String,
 }
 
 impl Config {
     pub fn new() -> Self {
         Self {
             addr: env::var("ADDR").unwrap_or("0.0.0.0:3000".to_string()),
+            db_conn: env::var("DB_CONN").unwrap_or("db connection is required".to_string()),
         }
     }
 }
